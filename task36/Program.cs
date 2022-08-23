81604de20d3a9046ad8ced3e7fb83b21dcc9cde1
«qwoizt» <boringfine@gmail.com>
2022-08-23 15:03:08 +0300

    задание 36

---
+++ task36/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i<array.Length;i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int FindSumm(int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i<array.Length; i++)
+    {
+        if (i % 2 !=0)
+        summ = summ + array[i];
+    }
+    return summ;
+}
+
+int[] array = CreateArray(5,1,99);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"сумма = {FindSumm(array)}");
