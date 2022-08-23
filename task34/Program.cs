59334f27bfbe1e0cd4601eda8d8f7de12b2a2891
«qwoizt» <boringfine@gmail.com>
2022-08-23 13:55:39 +0300

    задание 34

---
+++ task34/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i<array.Length;i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int FindChetnyeArray(int[] array)
+{
+   int amount = 0;
+   for (int i = 0; i<array.Length; i++)
+   {
+    if (array[i] % 2 == 0)
+        amount = amount + 1; 
+   } 
+   return amount;
+}
+
+
+int[] array = CreateArray(5, 100, 999);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"количество четных чисел {FindChetnyeArray(array)}");
